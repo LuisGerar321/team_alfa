ba102eed77b66d5e51ac2a19b1b1349eb89b8098
LuisGerar321 <dojas64@gmail.com>
2022-03-30 07:28:22 -0600

    feat: main develop

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "./max.h"
+#include "./min.h"
+#include "./odd.h"
+#define len 3
+
+int arr[len] = {3,1,10}; 
+
+int main(){
+    int indexMax = max(arr);
+    /// otras llamadas;
+    printf("max value:  %d\n", arr[indexMax]);
+
+    return 0;
+}
